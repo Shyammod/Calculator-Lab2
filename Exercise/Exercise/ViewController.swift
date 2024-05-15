521f4ab5f1ab3c90e4b956212321a99ea73e82ac
Shyammod <92877274+Shyammod@users.noreply.github.com>
2024-05-15 20:56:05 +0530

    initial commit

---
+++ Exercise/Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise
+//
+//  Created by user228349 on 5/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
